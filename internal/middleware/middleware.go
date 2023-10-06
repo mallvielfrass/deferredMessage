165714051d06e7fd9827e2c8b92698aab00283c5
mallvielfrass <jack@bykollab.com>
2023-10-06 18:46:04 +0300

    refactor middleware and add admin middleware

--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -2,6 +2,8 @@ package middleware
 
 import (
 	"deferredMessage/internal/db"
+	"deferredMessage/internal/db/mongo/session"
+	"fmt"
 	"net/http"
 	"time"
 
@@ -9,48 +11,98 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-type SessionMiddleware struct {
+type Middleware struct {
 	db db.DB
 }
 
-func InitSessionMiddleware(db db.DB) SessionMiddleware {
-	return SessionMiddleware{
+func InitMiddleware(db db.DB) Middleware {
+	return Middleware{
 		db: db,
 	}
 }
-func (n SessionMiddleware) CheckAuth() gin.HandlerFunc {
+func (n Middleware) checkSession(c *gin.Context) (*gin.Context, interface{}, bool) {
+	//get token from header
+	token := c.GetHeader("Authorization")
+	if token == "" {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "no token"})
+		return c, nil, false
+	}
+	//validate and convert token to primitive.ObjectID
+	sessionID, err := primitive.ObjectIDFromHex(token)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "invalid token"})
+		return c, nil, false
+	}
+
+	session, exist, err := n.db.Collections.Session.GetSessionByID(sessionID)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return c, nil, false
+	}
+	if !exist {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "invalid token"})
+		return c, nil, false
+	}
+
+	c.Set("session", session)
+
+	if session.Expire < time.Now().Unix() {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "session expired"})
+		return c, nil, false
+	}
+	if !session.Valid {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "invalid token"})
+		return c, nil, false
+	}
+	return c, session, true
+}
+func (n Middleware) CheckAuth() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		//get token from header
-		token := c.GetHeader("Authorization")
-		if token == "" {
-			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "no token"})
+		ctx, _, ok := n.checkSession(c)
+		c = ctx
+		if !ok {
+			if !c.IsAborted() {
+				c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "invalid session"})
+			}
 			return
 		}
-		//validate and convert token to primitive.ObjectID
-		sessionID, err := primitive.ObjectIDFromHex(token)
-		if err != nil {
+		c.Next()
+	}
+}
+func (n Middleware) CheckAdmin() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		sessionFromCtx, isExist := c.Get("session")
+		if !isExist {
+			ctx, session, ok := n.checkSession(c)
+			c = ctx
+			sessionFromCtx = session
+			if !ok {
+				if !c.IsAborted() {
+					c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "invalid session"})
+				}
+				return
+			}
+		}
+
+		session, ok := sessionFromCtx.(session.SessionScheme)
+
+		if !ok {
 			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "invalid token"})
 			return
 		}
-
-		session, exist, err := n.db.Collections.Session.GetSessionByID(sessionID)
+		user, isExist, err := n.db.Collections.User.GetUserByID(session.UserID)
+		fmt.Printf("user: %#v\n", user)
 		if err != nil {
 			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 			return
 		}
-		if !exist {
+		if !isExist {
 			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "invalid token"})
 			return
 		}
 
-		c.Set("session", session)
-		//	fmt.Printf("session: %v\n", session)
-		if session.Expire < time.Now().Unix() {
-			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "session expired"})
-			return
-		}
-		if !session.Valid {
-			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "invalid token"})
+		if !user.Admin {
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "you are not admin"})
 			return
 		}
 		c.Next()
