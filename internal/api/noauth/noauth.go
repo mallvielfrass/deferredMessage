9329224ad3cf58920882adeedd6345395ee7e87d
mallvielfrass <jack@bykollab.com>
2023-10-15 19:52:29 +0300

    add swagger api for noauth

--- internal/api/noauth/noauth.go
+++ internal/api/noauth/noauth.go
@@ -5,8 +5,6 @@ import (
 	"deferredMessage/internal/models"
 	"deferredMessage/internal/service"
 	"deferredMessage/internal/utils"
-	"deferredMessage/internal/utils/dto"
-	"fmt"
 	"net/http"
 	"time"
 
@@ -25,20 +23,19 @@ func Init(services *service.Service, middleware *middleware.Middleware) NoAuth {
 	}
 }
 
-// HandleCheckUserExist checks if a user already exists.
-//
-// @Summary Check if a user already exists
-// @Description Checks if a user with the provided email already exists
-// @Tags User
+// HandleCheckUserExist checks if a user exists.
+// @Summary Check if user exists
+// @Description Checks if a user exists based on the provided registration information.
+// @Tags NoAuth
 // @Accept json
 // @Produce json
-// @Param user body RegisterBody true "User details"
-// @Success 200 {object} StatusResponse{Status: string} "User does not exist"
+// @Param body body CheckUserRequest true "Check user request body"
+// @Success 200 {object} StatusResponse  "User does not exist"
+// @Success 202 {object} models.ErrorResponse "User already exists"
 // @Failure 400 {object} models.ErrorResponse "Bad request"
-// @Failure 202 {object} models.ErrorResponse "User already exists"
 // @Router /api/noauth/check [post]
 func (n NoAuth) HandleCheckUserExist(c *gin.Context) {
-	var body RegisterBody
+	var body CheckUserRequest
 	if err := c.ShouldBindJSON(&body); err != nil {
 		c.AbortWithStatusJSON(http.StatusBadRequest, models.ErrorResponse{
 			Error:  "no body",
@@ -58,108 +55,137 @@ func (n NoAuth) HandleCheckUserExist(c *gin.Context) {
 			Error: "user already exist"})
 		return
 	}
-	c.JSON(http.StatusOK, StatusResponse{Status: "not exist"})
+	c.JSON(http.StatusOK, StatusResponse{
+		Status: "not exist",
+	})
+
 }
 
+// HandleRegisterUser creates a new user.
+// @Summary Create a new user
+// @Description Creates a new user with the provided registration information.
+// @Tags NoAuth
+// @Accept json
+// @Produce json
+// @Param body body RegisterBody true "Registration information"
+// @Success 200 {object} RegisterUserResponse "User created successfully"
+// @Failure 400 {object} models.ErrorResponse "Bad request"
+// @Router /api/noauth/register [post]
+func (n NoAuth) HandleRegisterUser(c *gin.Context) {
+	//fmt.Println("register")
+	var body RegisterBody
+	if err := c.ShouldBindJSON(&body); err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, models.ErrorResponse{
+			Error:  "no body",
+			Reason: err.Error(),
+		})
+		return
+	}
+
+	isExist, err := n.services.UserService.CheckUserByMail(body.Mail)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, models.ErrorResponse{
+			Error: err.Error()})
+		return
+	}
+	if isExist {
+		c.AbortWithStatusJSON(http.StatusBadRequest, models.ErrorResponse{
+			Error: "user already exist"})
+		return
+	}
+	hash, err := utils.HashPassword(body.Password)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, models.ErrorResponse{
+			Error: err.Error()})
+		return
+	}
+	user, err := n.services.UserService.CreateUser(body.Name, body.Mail, hash)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, models.ErrorResponse{
+			Error: err.Error()})
+		return
+	}
+	userIP := c.ClientIP()
+	session, err := n.services.SessionService.CreateSession(user.ID, time.Now().Add(time.Hour*24*31).Unix(), userIP)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, models.ErrorResponse{
+			Error: err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, RegisterUserResponse{
+		Status: "success",
+		User: User{
+			Name: user.Name,
+			Mail: user.Mail,
+		},
+		Session: Session{
+			Id:     session.ID,
+			Expire: session.Expire,
+		},
+	})
+}
+
+// HandleLoginUser logs in a user.
+// @Summary Log in a user
+// @Description Logs in a user with the provided login information.
+// @Tags NoAuth
+// @Accept json
+// @Produce json
+// @Param body body LoginBody true "Login information"
+// @Success 200 {object} RegisterUserResponse "User logged in successfully"
+// @Failure 400 {object} models.ErrorResponse "Bad request"
+// @Router /api/noauth/login [post]
+func (n NoAuth) HandleLoginUser(c *gin.Context) {
+	var body LoginBody
+	if err := c.ShouldBindJSON(&body); err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, models.ErrorResponse{
+			Error:  "no body",
+			Reason: err.Error(),
+		})
+		return
+	}
+
+	user, isExist, err := n.services.UserService.GetUserByMail(body.Mail)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, models.ErrorResponse{
+			Error: "user or password incorrect"})
+
+		return
+	}
+	if !isExist {
+		c.AbortWithStatusJSON(http.StatusBadRequest, models.ErrorResponse{
+			Error: "user or password incorrect"})
+		return
+	}
+	if !utils.CheckPasswordHash(body.Password, user.Hash) {
+		c.AbortWithStatusJSON(http.StatusBadRequest, models.ErrorResponse{
+			Error: "user or password incorrect"})
+		return
+	}
+	userIP := c.ClientIP()
+	session, err := n.services.SessionService.CreateSession(user.ID, time.Now().Add(time.Hour*24*31).Unix(), userIP)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, models.ErrorResponse{
+			Error: err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, RegisterUserResponse{
+		Status: "success",
+		User: User{
+			Name: user.Name,
+			Mail: user.Mail,
+		},
+		Session: Session{
+			Id:     session.ID,
+			Expire: session.Expire,
+		},
+	})
+}
 func (n NoAuth) Router(router *gin.RouterGroup) *gin.RouterGroup {
 	r := router.Group("/")
-	r.POST("/check", func(c *gin.Context) {
-		//fmt.Println("register")
-		body, exist := dto.GetStruct[RegisterBody](c, RegisterBody{})
-		if !exist {
-			c.AbortWithStatusJSON(http.StatusBadRequest, models.ErrorResponse{
-				Error: "no body"})
-			return
-		}
-		isExist, err := n.services.UserService.CheckUserByMail(body.Mail)
-		if err != nil {
-			c.AbortWithStatusJSON(http.StatusBadRequest, models.ErrorResponse{
-				Error: err.Error()})
-			return
-		}
-		if isExist {
-			c.JSON(http.StatusBadRequest, models.ErrorResponse{
-				Error: "user already exist"})
-			return
-		}
-		c.JSON(http.StatusOK, StatusResponse{Status: "not exist"})
-	})
-
-	r.POST("/register", func(c *gin.Context) {
-		//fmt.Println("register")
-		body, exist := dto.GetStruct[RegisterBody](c, RegisterBody{})
-		if !exist {
-			c.AbortWithStatusJSON(http.StatusBadRequest, models.ErrorResponse{
-				Error: "no body"})
-			return
-		}
-
-		isExist, err := n.services.UserService.CheckUserByMail(body.Mail)
-		if err != nil {
-			c.AbortWithStatusJSON(http.StatusBadRequest, models.ErrorResponse{
-				Error: err.Error()})
-			return
-		}
-		if isExist {
-			c.AbortWithStatusJSON(http.StatusBadRequest, models.ErrorResponse{
-				Error: "user already exist"})
-			return
-		}
-		hash, err := utils.HashPassword(body.Password)
-		if err != nil {
-			c.AbortWithStatusJSON(http.StatusBadRequest, models.ErrorResponse{
-				Error: err.Error()})
-			return
-		}
-		user, err := n.services.UserService.CreateUser(body.Name, body.Mail, hash)
-		if err != nil {
-			c.AbortWithStatusJSON(http.StatusBadRequest, models.ErrorResponse{
-				Error: err.Error()})
-			return
-		}
-		userIP := c.ClientIP()
-		session, err := n.services.SessionService.CreateSession(user.ID, time.Now().Add(time.Hour*24*31).Unix(), userIP)
-		if err != nil {
-			c.AbortWithStatusJSON(http.StatusBadRequest, models.ErrorResponse{
-				Error: err.Error()})
-			return
-		}
-
-		c.JSON(http.StatusOK, gin.H{"status": "success", "user": gin.H{"name": user.Name, "mail": user.Mail}, "session": gin.H{"id": session.ID, "expire": session.Expire}})
-	})
-	r.POST("/login", func(c *gin.Context) {
-		body, exist := dto.GetStruct[LoginBody](c, LoginBody{})
-		if !exist {
-			c.AbortWithStatusJSON(http.StatusBadRequest, models.ErrorResponse{
-				Error: "no body"})
-			return
-		}
-
-		user, isExist, err := n.services.UserService.GetUserByMail(body.Mail)
-		if err != nil {
-			c.AbortWithStatusJSON(http.StatusBadRequest, models.ErrorResponse{
-				Error: "user or password incorrect"})
-			fmt.Println("/login error: ", err.Error())
-			return
-		}
-		if !isExist {
-			c.AbortWithStatusJSON(http.StatusBadRequest, models.ErrorResponse{
-				Error: "user or password incorrect"})
-			return
-		}
-		if !utils.CheckPasswordHash(body.Password, user.Hash) {
-			c.AbortWithStatusJSON(http.StatusBadRequest, models.ErrorResponse{
-				Error: "user or password incorrect"})
-			return
-		}
-		userIP := c.ClientIP()
-		session, err := n.services.SessionService.CreateSession(user.ID, time.Now().Add(time.Hour*24*31).Unix(), userIP)
-		if err != nil {
-			c.AbortWithStatusJSON(http.StatusBadRequest, models.ErrorResponse{
-				Error: err.Error()})
-			return
-		}
-		c.JSON(http.StatusOK, gin.H{"status": "success", "session": gin.H{"id": session.ID, "expire": session.Expire}})
-	})
+	r.POST("/check", n.HandleCheckUserExist)
+	r.POST("/register", n.HandleRegisterUser)
+	r.POST("/login", n.HandleLoginUser)
 	return r
 }
--- internal/api/noauth/types.go
+++ internal/api/noauth/types.go
@@ -12,6 +12,22 @@ type LoginBody struct {
 	Mail     string `json:"mail" binding:"required"`
 	Password string `json:"password" binding:"required"`
 }
+type CheckUserRequest struct {
+	Mail string `json:"mail"`
+}
 type StatusResponse struct {
 	Status string `json:"status"`
+}
+type User struct {
+	Name string `json:"name"`
+	Mail string `json:"mail"`
+}
+type Session struct {
+	Id     string `json:"_id"`
+	Expire int64  `json:"expire"`
+}
+type RegisterUserResponse struct {
+	Status  string  `json:"status"`
+	User    User    `json:"user"`
+	Session Session `json:"session"`
 }
