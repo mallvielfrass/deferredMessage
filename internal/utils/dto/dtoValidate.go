710612928bd3a67937b12c2c15e89bdbd8c7c9bc
mallvielfrass <jack@bykollab.com>
2023-10-16 11:55:47 +0300

    add dto limits and map utils

--- internal/utils/dto/dtoValidate.go
+++ internal/utils/dto/dtoValidate.go
@@ -1,6 +1,9 @@
 package dto
 
 import (
+	"reflect"
+	"strconv"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -23,10 +26,53 @@ func GetStruct[T any](c *gin.Context, body T) (T, bool) {
 // 	var body any
 // 	// using BindJson method to serialize body with struct
 
-// 	fmt.Println("validate: ", body)
-// 	if err := c.ShouldBindJSON(&body); err != nil {
-// 		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-// 		return
-// 	}
-// 	c.Set("body", body)
-// }
+//		fmt.Println("validate: ", body)
+//		if err := c.ShouldBindJSON(&body); err != nil {
+//			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+//			return
+//		}
+//		c.Set("body", body)
+//	}
+type limits struct {
+	Count  int `json:"count"`
+	Offset int `json:"offset"`
+}
+
+func GetLimits(c *gin.Context) (limits, error) {
+	lims := limits{
+		Count:  10,
+		Offset: 0,
+	}
+	countString := c.DefaultQuery("count", "10")
+	offsetString := c.DefaultQuery("offset", "0")
+	count, err := strconv.Atoi(countString)
+	if err != nil {
+		return lims, err
+	}
+	lims.Count = count
+	offset, err := strconv.Atoi(offsetString)
+	if err != nil {
+		return lims, err
+
+	}
+	lims.Offset = offset
+	return lims, nil
+
+}
+
+type ParamTyped struct {
+	Name string `json:"name"`
+	Type string `json:"type"`
+}
+
+func TypedMap(paramTyped []ParamTyped, body map[string]interface{}) map[string]interface{} {
+	filtred := make(map[string]interface{})
+	for _, param := range paramTyped {
+		if val, ok := body[param.Name]; ok {
+			if reflect.TypeOf(val).String() == param.Type {
+				filtred[param.Name] = val
+			}
+		}
+	}
+	return filtred
+}
